0260ee818561b0efda95c703157d0c4591d63266
bedibe <beadelino@gmail.com>
2022-10-19 19:11:55 -0300

    modificações

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,18 +1,3 @@
-function consultaEndereço(){
-   cep = document.querySelector('#cep').value
 
-  if (cep.length !== 8){
-    alert('CEP Inválido!');
-    return;
-  }
-  
-    url = "https://viacep.com.br/ws/"+ cep +"/json/";
-
-   fetch(url).then(function(response){
-    response.json().then(function(data){
-        console.log(data)
-    })
-   });
-}
    
 
